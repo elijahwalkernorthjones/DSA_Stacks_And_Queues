fae5a7f19da8b7ed71234987d14a5098a15f4647
Elijah Walker North Jones <elijahwalkernorthjones@gmail.com>
2024-01-29 10:40:08 -0500

    FINISHED BUILDING A QUEUEEEEEE

--- queue.js
+++ queue.js
@@ -23,11 +23,10 @@ class Queue {
     const newNode = new Node(val);
     if (!this.first){
       this.first = newNode;
-      this.last = newNode;
     } else {
-      this.last = newNode;
       this.last.next = newNode;
     }
+    this.last = newNode;
     this.size++;
   }
 
@@ -69,16 +68,31 @@ class Queue {
 
 const queue = new Queue();
 
+// Enqueue 3 nodes to our queue.
+queue.enqueue('firstttt is Tom');
+queue.enqueue('SECOND in nancy');
+queue.enqueue('thurd is choncy');
+
+//check size
+console.log("Queue size:", queue.size); // 3
+
+//Dequeue first node
+console.log("Dequeued:", queue.dequeue()); // Output: Dequeued: 'firstttt is Tom'
+
+console.log("Queue size:", queue.size); // 2?
 
 
 
+// Output the first element without removing it
+console.log("First element:", queue.peek()); // Output: First element: should be "SECOND is nancy"
 
 
+console.log(queue.dequeue()) // nancy
 
+console.log(queue.peek()) // choncy
 
-
-
-
+// check size
+console.log(queue.size) // should be 1
 
 
 
